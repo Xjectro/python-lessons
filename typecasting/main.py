7c57f7c6e4122d6bb4968b9a3a244c0319a21e8d
Xjectro <129968185+Xjectro@users.noreply.github.com>
2025-05-03 16:08:14 +0300

    I learned type casting

---
+++ typecasting/main.py
@@ -0,0 +1,20 @@
+# Typecasting = the process of converting a variable from one data type to another
+#               str(), int(), float(), bool()
+
+# If a variable is an empty string, it returns False, if it is full, it returns True.
+
+name = "Xjectro"
+age = 25
+gpa = 3.2
+is_student = True
+
+# print(type(name))
+
+# gpa = int(gpa)
+# print(gpa)
+
+# age = float(age)
+# print(age)
+
+age = str(age)
+print(type(age))
