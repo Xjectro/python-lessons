85448ebc0a8c52cb671cfc87b620a462e77d773a
Xjectro <129968185+Xjectro@users.noreply.github.com>
2025-05-03 17:38:13 +0300

    if statements

---
+++ if-statements/exercise1.py
@@ -0,0 +1,6 @@
+response = input("Would you like food? (Y/N): ")
+
+if response == "Y":
+    print("You have chosen food.")
+else:
+    print("You have not chosen food.")
---
+++ if-statements/main.py
@@ -0,0 +1,13 @@
+# if = Do some code only IF some condition is True
+#      Else do someting else
+
+age = int(input("Enter your age: "))
+
+if age >= 100:
+    print("You are a centenarian")
+elif age >= 18:
+    print("You are an adult")
+elif age < 0:
+    print("You are not born yet")
+else:
+    print("You are a minor")
