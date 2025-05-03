62e3c1d83c997daada2663b7faa927cceec50fdd
Xjectro <129968185+Xjectro@users.noreply.github.com>
2025-05-03 22:30:44 +0300

    edit return statement

--- functions/return_statement/exercise1.py
+++ functions/return_statement/exercise1.py
@@ -1,6 +1,7 @@
 def create_name(first_name, last_name):
     return f"{first_name.capitalize()} {last_name.capitalize()}"
 
-name = create_name("john", "doe")
 
-print(name)
+full_name = create_name("john", "doe")
+
+print(full_name)
