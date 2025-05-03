7e1dcd8f790f9b9267ebc46a88f83f50b2db297e
Xjectro <129968185+Xjectro@users.noreply.github.com>
2025-05-03 20:04:14 +0300

    We learned about for loops

---
+++ for-loops/main.py
@@ -0,0 +1,12 @@
+# for loops = execute a block of code fixed number of times.
+#             You can iterate over a range, string, sequence, etc.
+
+credit_card = "1234-5678-9012-3456"
+
+for x in range(1,21):
+    if x == 13:
+        break
+    else:
+        print(x)
+
+print("HAPPY NEW YEAR!")
