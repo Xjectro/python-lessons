9d64fe4f3296efde6401060bbdd57935f73173c5
Xjectro <129968185+Xjectro@users.noreply.github.com>
2025-05-03 17:25:02 +0300

    Arithmetic & Math

---
+++ arithmetic&math/main.py
@@ -0,0 +1,40 @@
+import math
+
+friends = 10
+
+# friends = friends + 1
+# friends += 1
+# friends = friends - 2
+# friends -= 2
+
+# friends = friends * 3
+# friends *= 3
+# friends = friends / 2
+# friends /= 2
+
+# friends = friends ** 2
+# friends **= 2
+
+# remainder = friends % 2
+
+# print(friends)
+
+x = 3.14
+y = 4
+z = 9
+
+# result = round(x)
+# result = abs(y)
+# result = pow(4, 3)
+# result = max(x, y, z)
+# result = min(x, y, z)
+
+# print(result)
+
+# print(math.pi)
+# print(math.e)
+# result = math.sqrt(z)
+# result = math.ceil(z)
+result = math.floor(z)
+
+print(result)
