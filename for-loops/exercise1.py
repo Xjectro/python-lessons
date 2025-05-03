f2983e2c030b9918e0bf4237bc5e6cd1c18fd9eb
Emre <157912176+Emre03477@users.noreply.github.com>
2025-05-03 22:15:08 +0300

    Update exercise1.py

---
+++ for-loops/exercise1.py
@@ -0,0 +1,20 @@
+x = 15 # How many lines do you want to print For example, we write 15 
+
+for e in range(1,x): # We create a loop starting from 1 and going through the number we wrote in the definition above. 
+    print('₺' * e)
+# Sample Output: 
+₺
+₺₺
+₺₺₺
+₺₺₺₺
+₺₺₺₺₺
+₺₺₺₺₺₺
+₺₺₺₺₺₺₺
+₺₺₺₺₺₺₺₺
+₺₺₺₺₺₺₺₺₺
+₺₺₺₺₺₺₺₺₺₺
+₺₺₺₺₺₺₺₺₺₺₺
+₺₺₺₺₺₺₺₺₺₺₺₺
+₺₺₺₺₺₺₺₺₺₺₺₺₺
+₺₺₺₺₺₺₺₺₺₺₺₺₺₺
+₺₺₺₺₺₺₺₺₺₺₺₺₺₺₺
