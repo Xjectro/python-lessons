5467a1cb77add5e90c506b7f830846aa9c94192a
Emre <157912176+Emre03477@users.noreply.github.com>
2025-05-03 22:44:42 +0300

    Update exercise2.py

---
+++ for-loops/exercise2.py
@@ -0,0 +1,3 @@
+for x in range(1, 4):  # We create a loop from 1 to 4 
+    for y in range(1, 4):  # In our second loop we create 1 to 4 
+        print(f"{x} x {y} = {x*y}") # We multiply the numbers coming from the loop with each other and print them on the screen.
