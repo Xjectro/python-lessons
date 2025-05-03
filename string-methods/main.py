2d013f4261b1d835afa8917c7424e48a4c520b6f
Xjectro <129968185+Xjectro@users.noreply.github.com>
2025-05-03 18:45:42 +0300

    We learned string methods and did exercises.

---
+++ string-methods/main.py
@@ -0,0 +1,18 @@
+# name = input("Enter your full name: ")
+phone_number = input("Enter your phone #: ")
+
+# result = len(name)
+# result = name.find("x")
+# result = name.rfind("x")
+# result = name.capitalize()
+# result = name.upper()
+# result = name.lower()
+# result = name.isdigit()
+# result = name.isalpha()
+
+# result = phone_number.count("-")
+# result = phone_number.replace("-"," ")
+
+# print(result)
+
+print(help(str))
