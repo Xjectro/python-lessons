98979db477059421487a48f366bcddf2b65d7b98
Xjectro <129968185+Xjectro@users.noreply.github.com>
2025-05-03 19:49:43 +0300

    We learned while loops

---
+++ while-loop/main.py
@@ -0,0 +1,9 @@
+# while loop = execute some code WHILE some condition remains true
+
+age = int(input("Enter your age: "))
+
+while age <= 0:
+    print("You are not born yet")
+    age = int(input("Enter your age: "))
+
+print(f"Your age {age} years old")
