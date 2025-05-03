3a5388e50d6ed247ff7bc0f515a9f17eec14e99d
Xjectro <129968185+Xjectro@users.noreply.github.com>
2025-05-03 18:54:45 +0300

    string indexing

--- string-indexing/main.py
+++ string-indexing/main.py
@@ -3,5 +3,15 @@
 
 credit_number = "1234-5678-9012-3456"
 
+credit_number = credit_number[::-1]
+print(credit_number)
+
+# last_digits = credit_number[-4:]
+# print(f"XXXX-XXXX-XXXX-{last_digits}")
+
 # print(credit_number[4])
 # print(credit_number[0:4])
+# print(credit_number[:4])
+# print(credit_number[4:])
+# print(credit_number[-1])
+# print(credit_number[::2])
