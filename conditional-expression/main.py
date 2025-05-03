bcb6063516d280a342433a0fd14d1920c34265d4
Xjectro <129968185+Xjectro@users.noreply.github.com>
2025-05-03 18:19:45 +0300

    We learned conditional expressions

---
+++ conditional-expression/main.py
@@ -0,0 +1,18 @@
+# Conditional Expression = A one-line shortcut for the if-else statement (ternary operator)
+#                          Print or assing one of two values based on a condition
+#                          X if condition else Y
+
+num = 5
+a = 6
+b = 7
+age = 13
+temperature = 30
+
+# print("Positive" if num > 0 else "Negative")
+# result = "EVEN" if num %2 == 0 else "ODD"
+# max_num = a if a > b else b
+# min_num = a if a < b else b
+# status = "Adult" if age >= 18 else "Child"
+weather = "HOT" if temperature >= 20 else "COLD"
+
+print(weather)
