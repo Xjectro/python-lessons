a9da8325ac86454b90fb8cc146ca66b46731b4f1
Xjectro <129968185+Xjectro@users.noreply.github.com>
2025-05-06 21:26:52 +0300

    Variables

---
+++ Sources/Variables/main.py
@@ -0,0 +1,19 @@
+# Strings
+username = "Xjectro"
+food = "Pasta"
+email = "xjectro@example.com"
+
+# Integers
+age = 25
+quantity = 10
+num_of_students = 30
+
+# Float
+price = 10.99
+gpa = 3.2
+distance = 5.5
+
+# Boolean
+is_student = True
+for_sale = False
+is_online = True
