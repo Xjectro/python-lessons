21af0330b67f3fe457ada17015a0fadb7dad4071
Xjectro <129968185+Xjectro@users.noreply.github.com>
2025-05-06 21:54:33 +0300

    Logical Operators

---
+++ Sources/Logical Operators/README.md
@@ -0,0 +1,7 @@
+# Logical Operators = evaluate multiple conditions (or, and, not)
+
+# or = at least one condition must be True
+
+# and = both conditions must be True
+
+# not = inverts the condition (not False, not True)
--- Sources/Logical Operators/main.py
+++ Sources/Logical Operators/main.py
@@ -1,8 +1,3 @@
-# Logical Operators = evaluate multiple conditions (or, and, not)
-#                    or  = at least one condition must be True
-#                    and = both conditions must be True
-#                    not = inverts the condition (not False, not True)
-
 temp = 20
 is_raining = False
 is_sunny = False
