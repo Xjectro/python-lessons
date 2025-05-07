e84dd98e8dd660b09c744937104708b392cc18a0
Xjectro <129968185+Xjectro@users.noreply.github.com>
2025-05-07 23:33:44 +0300

    PyQt5 Images

---
+++ Sources/PyQt5 Images/main.py
@@ -0,0 +1,31 @@
+import sys
+from PyQt5.QtWidgets import QApplication, QMainWindow, QLabel
+from PyQt5.QtGui import QIcon, QPixmap
+from PyQt5.QtCore import Qt
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("New PyQt5 GUI")
+        self.setGeometry(550, 250, 800, 600)
+        self.setWindowIcon(QIcon("Assets/PyQt5.png"))
+
+        label = QLabel(self)
+        label.setGeometry(0, 0, 800, 600)
+
+        pixmap = QPixmap("Assets/PyQt5.png")
+        label.setPixmap(pixmap)
+
+        label.setScaledContents(True)
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
