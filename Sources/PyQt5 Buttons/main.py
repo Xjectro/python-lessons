de5999f0e41fe95ee9d8971d5f8fbed8f962b184
Xjectro <129968185+Xjectro@users.noreply.github.com>
2025-05-08 17:15:58 +0300

    PyQt5 Buttons

--- Sources/PyQt5 Buttons/main.py
+++ Sources/PyQt5 Buttons/main.py
@@ -1,11 +1,8 @@
 import sys
-from PyQt5.QtWidgets import (
-    QApplication,
-    QMainWindow,
-   QPushButton,QLabel
-)
+from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QLabel
 from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.QtCore import Qt
+import time
 
 
 class MainWindow(QMainWindow):
@@ -13,11 +10,27 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("New PyQt5 GUI")
         self.setGeometry(550, 250, 800, 600)
+        self.label = QLabel("Hello", self)
+        self.button = QPushButton("Click Me", self)
         self.setWindowIcon(QIcon("Assets/PyQt5.png"))
         self.initUI()
 
     def initUI(self):
-        pass
+        self.button.setGeometry(
+            ((self.width() - 200) // 2), ((self.height() - 100) // 2), 200, 100
+        )
+        self.button.setStyleSheet("font-size: 20px;")
+        self.button.clicked.connect(self.on_click)
+
+        self.label.setGeometry(
+            ((self.width() - 200) // 2), ((self.height() - 200) // 2), 200, 50
+        )
+        self.label.setStyleSheet("font-size: 20px;")
+
+    def on_click(self):
+        print("Button clicked!")
+        self.label.setText("Goodbye")
+        self.button.setDisabled(True)
 
 
 def main():
