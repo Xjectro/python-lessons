f0ea0235ec14ef4e66d594baa2b448b902ab3b4a
Xjectro <129968185+Xjectro@users.noreply.github.com>
2025-05-08 16:46:55 +0300

    PyQt5 Buttons

--- Sources/PyQt5 Buttons/main.py
+++ Sources/PyQt5 Buttons/main.py
@@ -3,10 +3,6 @@ from PyQt5.QtWidgets import (
     QApplication,
     QMainWindow,
     QLabel,
-    QWidget,
-    QVBoxLayout,
-    QHBoxLayout,
-    QGridLayout,
 )
 from PyQt5.QtGui import QIcon, QPixmap
 from PyQt5.QtCore import Qt
@@ -21,33 +17,7 @@ class MainWindow(QMainWindow):
         self.initUI()
 
     def initUI(self):
-        central_widget = QWidget()
-        self.setCentralWidget(central_widget)
-
-        label1 = QLabel("#1", self)
-        label2 = QLabel("#2", self)
-        label3 = QLabel("#3", self)
-        label4 = QLabel("#4", self)
-        label5 = QLabel("#5", self)
-        label6 = QLabel("#6", self)
-
-        label1.setStyleSheet("background-color: red;")
-        label2.setStyleSheet("background-color: green;")
-        label3.setStyleSheet("background-color: blue;")
-        label4.setStyleSheet("background-color: yellow;")
-        label5.setStyleSheet("background-color: purple;")
-        label6.setStyleSheet("background-color: orange;")
-
-        grid = QGridLayout()
-
-        grid.addWidget(label1, 0, 0)
-        grid.addWidget(label2, 0, 1)
-        grid.addWidget(label3, 1, 0)
-        grid.addWidget(label4, 1, 1)
-        grid.addWidget(label5, 2, 0)
-        grid.addWidget(label6, 2, 1)
-
-        central_widget.setLayout(grid)
+     pass
 
 
 def main():
